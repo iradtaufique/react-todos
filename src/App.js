c17e19cf0ccbf49a673254b8d9006d980ee749e8
iradtaufique <iradukunda.ta@gmail.com>
2022-04-06 13:57:33 +0200

    adding models when you click on delete button

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text="Learn React"/>
-      <Todo text="Master React"/>
-      <Todo text="Getting started React"/>
+      <Todo text="Learn React" />
+      <Todo text="Master React two" />
+      <Todo text="Getting started React" />
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false); // using javascript states
+
+  function deleteHandler() {
+    setModalIsOpen(true); // call setModal and change status to true
+  }
+  function closeModalHandler() {
+    setModalIsOpen(false); // call setModal and change status to false
+  }
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
